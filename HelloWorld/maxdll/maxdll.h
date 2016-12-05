c5f6a043dea12f3ca53feb0c43bc2412e5b4dbcb
Hardy0816 <sunsheng886@126.com>
2016-12-05 14:14:46 +0800

    重新上传

---
+++ HelloWorld/maxdll/maxdll.h
@@ -0,0 +1,13 @@
+//------------------ maxdll.h ----------------  
+#pragma once;  
+ 
+//该宏完成在dll项目内部使用__declspec(dllexport)导出  
+//在dll项目外部使用时，用__declspec(dllimport)导入  
+//宏DLL_IMPLEMENT在maxdll.cpp中定义  
+#ifdef DLL_IMPLEMENT  
+#define DLL_API __declspec(dllexport)  
+#else  
+#define DLL_API __declspec(dllimport)  
+#endif  
+// 计算两个double的最大值
+DLL_API double Max(double x,double y);
