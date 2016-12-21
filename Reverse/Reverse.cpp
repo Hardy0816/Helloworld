a7a3bcdfcf3b60f91c7c5508434a5cadaaa19103
Hardy0816 <sunsheng886@126.com>
2016-12-21 11:14:56 +0800

    change the code of Reverse

--- Reverse/Reverse.cpp
+++ Reverse/Reverse.cpp
@@ -2,23 +2,26 @@
 
 int reverse(int x)
 {
-    int y = 0;
-    int m=0,n=0;
+    long long y = 0;
+    long m=0,n=0;
 	n = x;
     for(int i=0;abs(n)>0;i++)
     {
-        m=n%10;
+		m=n%10;
         n=n/10;
         y+=m;
 		if(abs(n)>0)
-        y*=10;
+        y=y*10;
+
     }
+	if(abs(y)>2147483647)
+		y=0;
 	return y;
 }
 
 int main()
 {
-	int data = 1534236469;
+	int data = -1534236469;
 	int reverse_re = 0;
 	reverse_re = reverse(data);
 }
